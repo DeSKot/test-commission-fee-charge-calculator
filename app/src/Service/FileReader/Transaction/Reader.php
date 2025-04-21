79fa401ad78ecc2fcfdb7689f1881cc386cb0828
Dmytro Bosyi <sgcore2015@gmail.com>
2025-04-21 21:58:53 +0300

    second

--- app/src/Service/FileReader/Transaction/Reader.php
+++ app/src/Service/FileReader/Transaction/Reader.php
@@ -3,17 +3,28 @@ declare(strict_types=1);
 
 namespace CommissionFeeCalculator\FileReader\Transaction;
 
+use CommissionFeeCalculator\Exceptions\CsvTransactionFileReaderException;
 use CommissionFeeCalculator\FileReader\Csv\Csv;
 use CommissionFeeCalculator\FileReader\RowDto;
 use Generator;
 
 class Reader extends Csv
 {
+
+    public function __construct(
+
+    )
+    {
+    }
+
     function setRowDto(array $rowData): RowDto
     {
         return new TransactionRowDto($rowData);
     }
 
+    /**
+     * @throws CsvTransactionFileReaderException
+     */
     public function display(Generator $rows): void
     {
         /**
@@ -21,7 +32,10 @@ class Reader extends Csv
          */
         foreach ($rows as $row) {
             if (!$row instanceof TransactionRowDto) {
+                throw new CsvTransactionFileReaderException("Invalid row data type.");
             }
+
+
         }
     }
 }
